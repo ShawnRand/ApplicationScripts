2be87844d5519a06df81b3b8cd50b09ca9a38b3d
ShawnRand <92928513+ShawnRand@users.noreply.github.com>
2022-03-30 11:26:29 +0200

    Add files via upload

---
+++ Detection Template.ps1
@@ -0,0 +1,24 @@
+﻿# Version to detect
+$Ver = '60.7.0'
+
+# Path to .exe
+$path = 'C:\Program Files\Mozilla Firefox\Firefox.exe'
+$TestPath = (test-path $path)
+#write-host = $TestPath
+
+if ($TestPath.Equals($true))
+{
+ $appVersion = (get-item $path).VersionInfo.FileVersion 
+#Write-host $appVersion
+if ($appVersion.Contains($Ver))
+    {
+    Write-host 'Installed'
+}
+else
+ {
+  }
+    }
+Else
+  {
+    Write-host 'nope'
+   }
